f07e70b0ed977b065ed968416d90ca00919b3ec6
fahim-shahriar-sqa <work.fahimshahriar@gmail.com>
2026-05-26 10:30:52 +0600

    Add Selenium dropdown interaction test using Select class

---
+++ src/test/java/fahimshahriar/HandleDropdownWithSelect.java
@@ -0,0 +1,26 @@
+package fahimshahriar;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.Test;
+
+public class HandleDropdownWithSelect extends DriverSetup{
+
+    @Test
+    public void interactWithDropdown() throws InterruptedException {
+
+        // Open Practice Website
+        driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+        WebElement selectElement = driver.findElement(By.cssSelector("#dropdown-class-example"));
+        Select select = new Select(selectElement);
+        selectElement.click();
+        select.selectByIndex(2);
+        Thread.sleep(3000);
+        select.selectByVisibleText("Option3");
+        Thread.sleep(3000);
+        select.selectByValue("option1");
+        Thread.sleep(3000);
+
+    }
+}
